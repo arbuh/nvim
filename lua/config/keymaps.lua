a6f85a1b455468cc2bd9942fc546f9d0cc13ebf0
Pavels <p.smirnov@outlook.com>
2022-10-04 21:43:05 +0200

    Adjust keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,7 @@ local keymap = vim.api.nvim_set_keymap
 local default_opts = { noremap = true, silent = true }
 
 -- Save/Close
-keymap('n', '<C-w>', ':w<CR>', default_opts)
-keymap('n', '<C-q>', ':q<CR>', default_opts)
+keymap('n', '<S-s>', ':w<CR>', default_opts)
 keymap('n', '<S-x>', ':qa<CR>', default_opts)
 
 -- Splits
@@ -13,6 +12,7 @@ keymap('n', '<C-k>', '<C-w><Up>', default_opts)
 keymap('n', '<C-l>', '<C-w><Right>', default_opts)
 keymap('n', '<C-s>', ':sp<CR>', default_opts)
 keymap('n', '<C-v>', ':vsp<CR>', default_opts)
+keymap('n', '<C-w>', ':q<CR>', default_opts) -- use this command to close a split, otherwise use <S-w> for tabs clossing
 
 -- Buffers
 keymap('n', '<S-h>', ':bprevious<CR>', default_opts)
