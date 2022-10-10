189d46ddb97988e73dc906d8cf37b31c9ac913e7
Pavels <p.smirnov@outlook.com>
2022-10-10 22:04:01 +0200

    Uncomment telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -87,30 +87,31 @@ local function plugins(use)
         use 'ibhagwan/fzf-lua'
     end
 
-    -- Telescope (advanced search)
-    --[[
-    use {
-        'nvim-telescope/telescope.nvim',
-        config = function()
-            require('config.telescope')
-        end,
-        wants = {
-            'plenary.nvim',
-            'popup.nvim',
-            'telescope-fzf-native.nvim',
-            'telescope-project.nvim',
-            'telescope-repo.nvim',
-            'project.nvim',
-        },
-        requires = {
-            'nvim-lua/popup.nvim',
-            'nvim-lua/plenary.nvim',
-            { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
-            'nvim-telescope/telescope-project.nvim',
-            'cljoly/telescope-repo.nvim',
-            'ahmedkhalf/project.nvim',
-        },
-    } ]]--
+    -- Telescope (another fuzzy search plugin)
+    if PLUGINS.fzf_lua.enabled then
+        use {
+            'nvim-telescope/telescope.nvim',
+            config = function()
+                require('config.telescope')
+            end,
+            wants = {
+                'plenary.nvim',
+                'popup.nvim',
+                'telescope-fzf-native.nvim',
+                'telescope-project.nvim',
+                'telescope-repo.nvim',
+                'project.nvim',
+            },
+            requires = {
+                'nvim-lua/popup.nvim',
+                'nvim-lua/plenary.nvim',
+                { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+                'nvim-telescope/telescope-project.nvim',
+                'cljoly/telescope-repo.nvim',
+                'ahmedkhalf/project.nvim',
+            },
+        }
+    end
 
     -- Automatically set up configuration after cloning packer.nvim
     if packer_bootstrap then
