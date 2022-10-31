3197189b4df6365ee3af3e343ba4ccce245da0ef
Pavels <p.smirnov@outlook.com>
2022-10-31 09:47:03 +0100

    Add support of Java to Metals

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -25,7 +25,7 @@ metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 -- Autocmd to start Metals
 local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "scala", "sbt" },
+    pattern = { "scala", "sbt", "java" },
     callback = function()
       metals.initialize_or_attach(metals_config)
     end,
