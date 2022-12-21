838d59850c496d5cdc05c7cdde40b5684a93a61c
Pavels <p.smirnov@outlook.com>
2022-12-21 22:27:11 +0100

    Fix formatting

--- lua/config/general.lua
+++ lua/config/general.lua
@@ -18,7 +18,7 @@ opt.tabstop = 4                      -- number of columns occupied by a tab
 opt.shiftwidth = 4                   -- width for autoindents
 opt.expandtab = true                 -- converts tabs to white space
 opt.shiftwidth = 4                   -- width for autoindents
-vim.cmd('syntax on')                -- syntax highlighting
+vim.cmd('syntax on')                 -- syntax highlighting
 
 -- Leader
 g.mapleader = ';'
