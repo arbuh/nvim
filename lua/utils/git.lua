c41d10c98f095627ce3edb268d27a7767a26bc5a
Pavels <p.smirnov@outlook.com>
2023-06-07 22:32:06 +0200

    Fix diff with origin

--- lua/utils/git.lua
+++ lua/utils/git.lua
@@ -6,7 +6,7 @@ function git.diff_with_main()
     local default_branch = vim.fn.system("git remote show origin | sed -n '/HEAD branch/s/.*: //p'")
     local cleaned = string.gsub(default_branch, "\n", "")
 
-    diffview.open("origin/"..cleaned.."...HEAD")
+    diffview.open("origin/"..cleaned)
 end
 
 return git
