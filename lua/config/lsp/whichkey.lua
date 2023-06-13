de22514ab8f1718645ff5dda7c04dae1d8403a0a
Pavels <p.smirnov@outlook.com>
2023-06-13 15:41:53 +0200

    Simplify the code actions key sequence

--- lua/config/lsp/whichkey.lua
+++ lua/config/lsp/whichkey.lua
@@ -7,12 +7,14 @@ local opts = {
 
 local mappings = {
     ['h'] = { "<Cmd>lua vim.lsp.buf.hover()<CR>", "Hover" },
-    ['D'] = { "<Cmd>lua vim.lsp.buf.definition()<CR>", "Definition" },
+    ['D'] = { "<Cmd>lua vim.lsp.buf.definition()<CR>", "Go to definition" },
+    ['C'] = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "Code actions" },
     l = {
         name = "LSP",
         u = { "<cmd>lua vim.lsp.buf.references()<CR>", "References" },
         r = { "<cmd>lua vim.lsp.buf.rename()<CR>", "Rename" },
-        c = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "Code actions" },
+        -- c = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "Code actions" },
+        -- d = { "<Cmd>lua vim.lsp.buf.definition()<CR>", "Go to definition" },
         s = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature help" },
         f = { "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", "Formatting" },
         d = { "<cmd>lua vim.diagnostic.setqflist()<CR>", "Diagnostics" },
