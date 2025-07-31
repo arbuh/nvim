16355ba2a354a4d51e3d061149aeab96f60a2653
Pavels <p.smirnov@outlook.com>
2025-07-31 21:26:27 +0200

    Change the color scheme

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,11 @@
+-- Color scheme
+return {
+	"catppuccin/nvim",
+	config = function()
+		require("catppuccin").setup({
+			flavour = "macchiato",
+			show_end_of_buffer = true,
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
+}
