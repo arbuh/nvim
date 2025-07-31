0e5f4cd79850aa843ea823f41263e27a58f5fa7c
Pavels <p.smirnov@outlook.com>
2025-07-31 14:13:58 +0200

    Remove the safe cmp select because it doesn't work for the first items

--- lua/config/plugins/cmp.lua
+++ lua/config/plugins/cmp.lua
@@ -26,17 +26,7 @@ cmp.setup({
 			end
 		end,
 		-- Safely use Enter to chose the selected item
-		["<CR>"] = cmp.mapping({
-			i = function(fallback)
-				if cmp.visible() and cmp.get_active_entry() then
-					cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-				else
-					fallback()
-				end
-			end,
-			s = cmp.mapping.confirm({ select = true }),
-			c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-		}),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
 	}),
 })
 
