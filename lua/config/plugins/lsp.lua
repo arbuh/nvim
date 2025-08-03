9db03db4c0e8b2f1abeeee1ef74a3e7b4920c8a4
Pavels <p.smirnov@outlook.com>
2025-08-03 15:08:56 +0200

    Fix syntax highlight

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -8,3 +8,34 @@ mason_lspconfig.setup({
 	ensure_installed = { "gopls", "jdtls", "lua_ls", "pyright" },
 	automatic_enable = true,
 })
+
+-- Language specific config
+-- Find all the available configs here: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+local servers = {
+	gopls = {
+		settings = {
+			gopls = {
+				analyses = {
+					unusedparams = true,
+				},
+				staticcheck = true,
+				semanticTokens = true,
+			},
+		},
+	},
+	pyright = {
+		settings = {
+			python = {
+				analysis = {
+					typeCheckingMode = "basic",
+					semanticHighlighting = true,
+				},
+			},
+		},
+	},
+}
+
+local lspconfig = require("lspconfig")
+for server, config in pairs(servers) do
+	lspconfig[server].setup(config)
+end
