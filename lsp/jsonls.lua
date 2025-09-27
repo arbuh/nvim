7a7f355c46ac2bc2e4f1279adb39ca0d2d3bc4c5
pavels <p.smirnov@outlook.com>
2025-09-27 13:16:23 +0200

    Clean up

--- lsp/jsonls.lua
+++
@@ -1,6 +0,0 @@
--- Find all the available configs here: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
-return {
-	cmd = { "vscode-json-language-server", "--stdio" },
-	filetypes = { "json", "jsonc" },
-	root_markers = { ".git" },
-}
