04aacd8c286bdfe76070e62261d586f1c9c16a70
pavels <p.smirnov@outlook.com>
2025-10-24 11:54:01 +0200

    Make the file tree width adaptive

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -7,12 +7,18 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	opts = {
+		view = {
+			width = {
+				min = 30,
+				max = -1,
+			},
+		},
 		update_focused_file = {
 			enable = true,
 			update_root = false,
 		},
-        filters = {
-            git_ignored = false,
-        }
+		filters = {
+			git_ignored = false,
+		},
 	},
 }
