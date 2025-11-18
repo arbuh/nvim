dd6eb8caec98cb95f625853928564380cd03d6a4
pavels <p.smirnov@outlook.com>
2025-11-18 13:38:58 +0100

    Add configs for Java LSP server

---
+++ lsp/jdtls.lua
@@ -0,0 +1,13 @@
+-- Find all the available configs here: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+return {
+	cmd = { "jdtls" },
+	filetypes = { "java" },
+	root_markers = {
+		{ "mvnw", "gradlew", "settings.gradle", "settings.gradle.kts", ".git" },
+		{ "build.xml", "pom.xml", "build.gradle", "build.gradle.kts" },
+	},
+	settings = {
+		-- https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+		java = {},
+	},
+}
