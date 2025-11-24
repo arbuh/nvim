f5ab7eeb3929a39fb3fb13fa0353a1185c9b3859
pavels <p.smirnov@outlook.com>
2025-11-24 15:25:49 +0100

    Add checksum for the spingbootdemo gradle wrapper

--- lsp/jdtls.lua
+++ lsp/jdtls.lua
@@ -8,6 +8,22 @@ return {
 	},
 	settings = {
 		-- https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-		java = {},
+		java = {
+			imports = {
+				gradle = {
+					wrapper = {
+						checksums = {
+							{
+								sha256 = "423cb469ccc0ecc31f0e4e1c309976198ccb734cdcbb7029d4bda0f18f57e8d9",
+								allowed = true,
+							},
+						},
+					},
+				},
+			},
+		},
+		eclipse = {
+			downloadSources = true,
+		},
 	},
 }
