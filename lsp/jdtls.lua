a08044ec916c831c3ceda2a1662e67a850a06321
pavels <p.smirnov@outlook.com>
2025-11-28 14:55:13 +0100

    Edit jdtls configs

--- lsp/jdtls.lua
+++ lsp/jdtls.lua
@@ -9,16 +9,9 @@ return {
 	settings = {
 		-- https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
 		java = {
-			imports = {
+			import = {
 				gradle = {
-					wrapper = {
-						checksums = {
-							{
-								sha256 = "423cb469ccc0ecc31f0e4e1c309976198ccb734cdcbb7029d4bda0f18f57e8d9",
-								allowed = true,
-							},
-						},
-					},
+					enabled = true,
 				},
 			},
 		},
